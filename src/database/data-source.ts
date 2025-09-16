1ab2cef060f1fb4ffca9a9794f91e8f15407bb97
Hanif Ryanas <hanifryanas@gmail.com>
2025-09-16 12:18:49 +0700

    feat: updated user-roles

---
+++ src/database/data-source.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+config({ quiet: true });
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  entities: [join(__dirname, '../modules/**/entities/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, 'migrations/*.ts')],
+  synchronize: false,
+});
--- src/modules/user/enums/user-role.enum.ts
+++ src/modules/user/enums/user-role.enum.ts
@@ -1,4 +1,13 @@
 export enum UserRole {
-  Admin = 'admin',
   User = 'user',
+  Staff = 'staff',
+  Admin = 'admin',
+  SuperAdmin = 'superadmin',
 }
+
+export const RoleHierarchy = {
+  [UserRole.User]: 0,
+  [UserRole.Staff]: 1,
+  [UserRole.Admin]: 2,
+  [UserRole.SuperAdmin]: 3,
+};
