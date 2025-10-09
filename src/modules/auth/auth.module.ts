2e6b0e4e3e08e05db44867afbb16a2f488014db9
Hanif Ryanas <hanifryanas@gmail.com>
2025-10-09 20:09:30 +0700

    chore: fix typo jwt-strategies

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'modules/user/user.module';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
-import { JwtStrategy } from './strategies/jwt.stategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
