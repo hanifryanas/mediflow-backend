f9d663a9657bfce74ec8fedb77c30888905a1668
Hanif Ryanas <hanifryanas@gmail.com>
2025-10-14 21:36:57 +0700

    feat: reorganize appointment module imports and add controller

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Doctor } from 'modules/doctor/entities/doctor.entity';
-import { Appointment } from './entities/appointment.entity';
-import { Patient } from 'modules/patient/entities/patient.entity';
 import { Nurse } from 'modules/nurse/entities/nurse.entity';
+import { Patient } from 'modules/patient/entities/patient.entity';
+import { AppointmentController } from './controllers/appointment.controller';
+import { Appointment } from './entities/appointment.entity';
 import { AppointmentService } from './services/appoinment.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Appointment, Doctor, Patient, Nurse])],
   providers: [AppointmentService],
-  controllers: []
+  controllers: [AppointmentController]
 })
-export class AppointmentModule {}
+export class AppointmentModule { }
